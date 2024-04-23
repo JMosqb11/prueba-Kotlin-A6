638d069e0336789f1759253f0fe9518a2e1dc69e
Jhonier Mosquera <jhoniermosquera61@gmail.com>
2024-04-23 16:56:07 -0500

    first result

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,26 @@
 fun main() {
     println("Hola Mundo")
 }
+
+open class Phone(private var isScreenLightOn: Boolean = false){
+    fun switchOn() {
+        isScreenLightOn = true
+    }
+
+    fun switchOff() {
+        isScreenLightOn = false
+    }
+
+    fun checkPhoneScreenLight() {
+        val phoneScreenLight = if (isScreenLightOn) "on" else "off"
+        println("The phone screen's light is $phoneScreenLight.")
+    }
+}
+
+class FoldablePhone : Phone() {
+
+    private val folding: Boolean = false
+    fun noFolding() {
+        if(!folding) "pantalla encendida, teléfono no plegado" else "pantalla apagada, teléfono plegado"
+    }
+}
